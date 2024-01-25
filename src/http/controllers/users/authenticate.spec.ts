572f08cda3ec950b4ba5015115685a6361a30f95
Vinícius Massari <stonefields11@hotmail.com>
2024-01-25 08:57:24 -0300

    :white_check_mark: adding authenticate e2e test

---
+++ src/http/controllers/users/authenticate.spec.ts
@@ -0,0 +1,58 @@
+import {
+  describe,
+  it,
+  expect,
+  afterAll,
+  beforeEach,
+  beforeAll,
+  afterEach,
+} from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
+import { prisma } from '@/lib/prisma';
+describe('Authenticate user test', async () => {
+  const userTestInfo = {
+    name: 'testName',
+    lastname: 'testLastName',
+    email: 'test@example.com',
+    password: 'test1234',
+  };
+  beforeAll(async () => {
+    await app.ready();
+  });
+  beforeEach(async () => {
+    await prisma.user.deleteMany();
+  });
+
+  afterEach(async () => {
+    await prisma.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await prisma.user.deleteMany();
+
+    await app.close();
+  });
+
+  it('should authenticate an user', async () => {
+    await request(app.server).post('/user').send({
+      name: userTestInfo.name,
+      lastname: userTestInfo.lastname,
+      email: userTestInfo.email,
+      password: userTestInfo.password,
+    });
+
+    const response = await request(app.server).post('/user/authenticate').send({
+      email: userTestInfo.email,
+      password: userTestInfo.password,
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+    expect(response.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken='),
+    ]);
+  });
+});
